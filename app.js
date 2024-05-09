92d625e6490f9be049ec48d5dc8f4d75641ce319
Liz <132317396+mintep1-student@users.noreply.github.com>
2024-05-09 15:24:42 +1200

    chore: add comments for error handling

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use('/', homeRoutes);
 app.use('/api/agents', agentRoutes);
 
-
+// Sets 404 error message if request contains an invalid route and sends to next middleware function in the stack
 app.use((req, res, next) => {
   next(
     res.status(404).json({
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
   );
 });
 
+// Displays a written error message depending on the error found
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.send(err.message);
