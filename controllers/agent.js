11b881166c42007322621e496ad93530eebe0bc3
mintep1-student <mintep1@student.op.ac.nz>
2024-05-17 11:33:21 +1200

    feat: add handle that checks if user has entered all fields or not and displays an error message

--- controllers/agent.js
+++ controllers/agent.js
@@ -4,18 +4,32 @@ const prisma = new PrismaClient();
 const register = async (req, res) => {
   try {
     // Store given data
-    const {
-      password,
-      username,
-    } = req.body;
+    const { password, username } = req.body;
 
     // Create new user with the given data
     user = await prisma.user.create({
       data: {
         password,
         username,
+        confirm_password
       },
     });
+
+    // Define an array of required fields
+    const requiredFields = [
+      "username",
+      "password",
+      "confirm_password",
+    ];
+
+    // Check if all required fields are provided
+    const missingFields = requiredFields.filter((field) => !req.body[field]);
+
+    if (missingFields.length > 0) {
+      return res.status(400).json({
+        error: `Missing required fields: ${missingFields.join(", ")}`,
+      });
+    }
 
     // Return success or error message
     return res.status(201).json({
