e6072f23dcdc7f6ada310918bafd1c98d9c2c48a
mintep1-student <mintep1@student.op.ac.nz>
2024-05-17 14:21:48 +1200

    feat: stores user input for creating a contract chore: prisma migrate fix: adds default values to contract model

--- controllers/createContract.js
+++ controllers/createContract.js
@@ -3,8 +3,25 @@ const prisma = new PrismaClient();
 
 const createContract = async (req, res) => {
   try {
-    
+    // Store given data
+    const { type, deadline, payment, cargo, contractId, destinationSymbol, deadlineToAccept } = req.body;
 
+    // Create new user with the given data
+    user = await prisma.user.create({
+      data: {
+        type,
+        terms: {
+            create: [
+                deadline,
+                payment,
+                cargo,
+                contractId,
+            ]
+        },
+        destinationSymbol,
+        deadlineToAccept,
+      },
+    });
 
     // Return success or error message
     return res.status(201).json({
