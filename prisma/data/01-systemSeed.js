d3a5cc58eb8269dcd8ff47e0e8bdc15950b715f1
mintep1-student <mintep1@student.op.ac.nz>
2024-05-17 14:38:39 +1200

    feat: creates seed function chore: creates system seed data

---
+++ prisma/data/01-systemSeed.js
@@ -0,0 +1,13 @@
+const name = "system";
+const data = [{"name":"Hymenophyllaceae","type":"NEUTRON_STAR"},
+{"name":"Rubiaceae","type":"ORANGE_STAR"},
+{"name":"Asteraceae","type":"WHITE_DWARF"},
+{"name":"Fabaceae","type":"NEBULA"},
+{"name":"Cactaceae","type":"UNSTABLE"},
+{"name":"Crassulaceae","type":"NEBULA"},
+{"name":"Poaceae","type":"UNSTABLE"},
+{"name":"Poaceae","type":"NEBULA"},
+{"name":"Orobanchaceae","type":"UNSTABLE"},
+{"name":"Asteraceae","type":"YOUNG_STAR"}];
+
+export { name, data };
---
+++ prisma/seed.js
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client"
+
+const seeds = ["./data/01-participantSeed.js", "./data/02-animalSeed.js"]
+const prisma = new PrismaClient()
+
+const seed = async () => {
+  try {
+    for (let i = 0; i < seeds.length; i++) {
+      let { name, data } = await import(seeds[i])
+      await prisma[name].createMany({
+        data: data,
+      })
+    }
+
+    console.log("Database successfully seeded")
+
+    await prisma.$disconnect() // Disconnect from the database
+  } catch (err) {
+    console.error(err)
+    await prisma.$disconnect()
+    process.exit(1) // Exit the process
+  }
+}
+
+seed()
