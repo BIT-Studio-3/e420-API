8fca53eff0c1cbc6b9bda67f3a6b9e8511c6e799
mintep1-student <mintep1@student.op.ac.nz>
2024-05-20 16:15:45 +1200

    feat: creates waypoint seed data fix: change waypoint shipyard type to array

---
+++ prisma/data/02-waypointSeed.js
@@ -0,0 +1,2 @@
+const name = "system";
+const data = ;
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client"
 
-const seeds = ["./data/01-systemSeed.js", "./data/02-waypointSeed.js"]
+const seeds = ["./data/01-systemSeed.js"]
 const prisma = new PrismaClient()
 
 const seed = async () => {
