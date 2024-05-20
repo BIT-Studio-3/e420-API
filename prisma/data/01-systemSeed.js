4e7c87cc05e5de7d4e23fa08f169817fac979df2
mintep1-student <mintep1@student.op.ac.nz>
2024-05-20 16:36:44 +1200

    seeded successfully

--- prisma/data/01-systemSeed.js
+++ prisma/data/01-systemSeed.js
@@ -1,13 +1,15 @@
-const name = "system";
-const data = [{"name":"Hymenophyllaceae","type":"NEUTRON_STAR"},
-{"name":"Rubiaceae","type":"ORANGE_STAR"},
-{"name":"Asteraceae","type":"WHITE_DWARF"},
-{"name":"Fabaceae","type":"NEBULA"},
-{"name":"Cactaceae","type":"UNSTABLE"},
-{"name":"Crassulaceae","type":"NEBULA"},
-{"name":"Poaceae","type":"UNSTABLE"},
-{"name":"Poaceae","type":"NEBULA"},
-{"name":"Orobanchaceae","type":"UNSTABLE"},
-{"name":"Asteraceae","type":"YOUNG_STAR"}];
+const name = "System";
+const data = [
+  { name: "Hymenophyllaceae", type: "NEUTRON_STAR" },
+  { name: "Rubiaceae", type: "ORANGE_STAR" },
+  { name: "Asteraceae", type: "WHITE_DWARF" },
+  { name: "Fabaceae", type: "NEBULA" },
+  { name: "Cactaceae", type: "UNSTABLE" },
+  { name: "Crassulaceae", type: "NEBULA" },
+  { name: "Poaceae", type: "UNSTABLE" },
+  { name: "Luniceae", type: "NEBULA" },
+  { name: "Orobanchaceae", type: "UNSTABLE" },
+  { name: "Cosminiceae", type: "YOUNG_STAR" },
+];
 
 export { name, data };
