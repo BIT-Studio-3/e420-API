517e6812b46fb804ebea605a9d61a0f08d866be8
Liz <132317396+mintep1-student@users.noreply.github.com>
2024-05-20 17:15:03 +1200

    comments

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,8 +3,12 @@ import { PrismaClient } from "@prisma/client"
 const seeds = ["./data/01-systemSeed.js"]
 const prisma = new PrismaClient()
 
+/**
+* Seeds data in JSON format from any file provided in the seeds variable
+*/
 const seed = async () => {
   try {
+    // Import and seed data from each file in the seeds variable
     for (let i = 0; i < seeds.length; i++) {
       let { name, data } = await import(seeds[i])
       await prisma[name].createMany({
