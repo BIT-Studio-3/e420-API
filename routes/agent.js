4452aab900c5a171e8d813049591ae5e252b3bda
Liz <132317396+mintep1-student@users.noreply.github.com>
2024-05-22 13:30:46 +1200

    Update agent.js route

--- routes/agent.js
+++ routes/agent.js
@@ -7,8 +7,11 @@ import * as resources from "../controllers/agent.js";
 // Create an Express router
 const router = express.Router();
 
-// Create a GET route
+// Create a POST route
 router.post("/", resources.register);
+
+// Create a GET route
+router.get("/", (req, res) => resources.getAgents(req, res));
 
 // Export the router
 export default router;
