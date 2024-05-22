12aaa94e64635e8bf498ce7a6f28fe957c831c1d
mintep1-student <mintep1@student.op.ac.nz>
2024-05-22 13:24:48 +1200

    Revert "chore: merge contract functions into one contract file"
    This reverts commit 9a1f897f0c296c4f62cc7b00087124c9bb93dca9.

--- controllers/createContract.js
+++ controllers/createContract.js
@@ -36,24 +36,4 @@ const createContract = async (req, res) => {
   }
 };
 
-const getContracts = async (req, res) => {
-    try{
-        // get all the contracts associated with the user id
-        const contracts = await prisma.contract.findMany({
-            //query to get user specific contracts
-            where: userId === req.params.userId,
-        });
-        // error handling for finding no contracts
-        if (contracts.length === 0) {
-            return res.status(404).json({ msg: "No contracts found"})
-        }
-        return res.status(200).json({ msg: "Successfully fetched all contracts", data: contracts });
-    //handles any other errors
-    } catch (err) {
-        return res.status(500).json({
-            msg: err.message,
-        })
-    }
-};
-
-export { createContract, getContracts };
+export { createContract };
