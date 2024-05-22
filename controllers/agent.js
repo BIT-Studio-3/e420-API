137edeb78c04735ede2f542df3acface27d746a9
Liz <132317396+mintep1-student@users.noreply.github.com>
2024-05-22 13:14:19 +1200

    chore: move getAgents function into agent controller file

--- controllers/agent.js
+++ controllers/agent.js
@@ -1,49 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-const register = async (req, res) => {
-  try {
-    // Store given data
-    const { password, username } = req.body;
+const getAgents = async (req, res) => {
+    try {
+        //fetch all agents from the database
+      const agents = await prisma.user.findMany({});
 
-    // Create new user with the given data
-    user = await prisma.user.create({
-      data: {
-        password,
-        username,
-        confirm_password,
-      },
-    });
+        //if no agents are found return a 404 error
+      if (agents.length === 0) {
+        return res.status(404).json({ msg: "No agents found" });
+      }
+      //If agents are found, return a 200 success response with the agents data
+      return res.status(200).json({ msg: "Successfully fetched all agents", data: agents });
 
-    // Define an array of required fields
-    const requiredFields = ["username", "password", "confirm_password"];
-
-    // Check if all required fields are provided
-    const missingFields = requiredFields.filter((field) => !req.body[field]);
-
-    if (missingFields.length > 0) {
-      return res.status(400).json({
-        error: `Missing required fields: ${missingFields.join(", ")}`,
+      // Handle errors by returning a 500 error with the error message
+    } catch (err) {
+      return res.status(500).json({
+        msg: err.message,
       });
     }
-
-    // Check if user with given details already exists
-    let user = await prisma.user.findUnique({
-      where: { username: username },
-    });
-
-    if (user) return res.status(409).json({ msg: "User already exists" });
-
-    // Return success or error message
-    return res.status(201).json({
-      msg: "User successfully registered",
-      data: user,
-    });
-  } catch (err) {
-    return res.status(500).json({
-      msg: err.message,
-    });
-  }
-};
-
-export { register };
+  };
+export { getAgents };
