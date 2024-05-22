34e0788801bcebeaf79f6935f4497a7de0a681b7
Liz <132317396+mintep1-student@users.noreply.github.com>
2024-05-22 13:34:07 +1200

    fix: remove post route

--- routes/agent.js
+++ routes/agent.js
@@ -7,9 +7,6 @@ import * as resources from "../controllers/agent.js";
 // Create an Express router
 const router = express.Router();
 
-// Create a POST route
-router.post("/", resources.register);
-
 // Create a GET route
 router.get("/", (req, res) => resources.getAgents(req, res));
 
