a39589089fdda529704e172f51508786130e5fad
mintep1-student <mintep1@student.op.ac.nz>
2024-05-28 13:11:32 +1200

    get all systems and return success message

--- controllers/system.js
+++ controllers/system.js
@@ -3,6 +3,11 @@ const prisma = new PrismaClient();
 
 const getSystems = async (req, res) => {
   try {
+    const systems = await prisma.system.findMany({});
+
+    return res
+      .status(200)
+      .json({ msg: "Successfully fetched all systems", data: systems });
 
   } catch (err) {
     return res.status(500).json({
