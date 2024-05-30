f9d263018f3f97edf1ec33094810195166786ebc
mintep1-student <mintep1@student.op.ac.nz>
2024-05-31 11:55:03 +1200

    add error handling

--- controllers/waypoint.js
+++ controllers/waypoint.js
@@ -5,6 +5,15 @@ const getWaypoints = async (req, res) => {
   try {
     const waypoints = await prisma.waypoint.findMany({});
 
+    //if no waypoints are found return a 404 error
+    if (waypoints.length === 0) {
+      return res.status(404).json({ msg: "No waypoints found" });
+    }
+    
+    //If waypoints are found, return a 200 success response with the waypoints data
+    return res
+      .status(200)
+      .json({ msg: "Successfully fetched all waypoints", data: waypoints });
   } catch (err) {
     return res.status(500).json({
       msg: err.message,
