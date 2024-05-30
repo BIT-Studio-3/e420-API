20179f380376ff3caa592ef974cb7a3a211b714b
mintep1-student <mintep1@student.op.ac.nz>
2024-05-31 11:56:31 +1200

    fixed findMany function

--- controllers/waypoint.js
+++ controllers/waypoint.js
@@ -3,13 +3,20 @@ const prisma = new PrismaClient();
 
 const getWaypoints = async (req, res) => {
   try {
-    const waypoints = await prisma.waypoint.findMany({});
+    //fetch all waypoints from the database
+    const waypoints = await prisma.waypoint.findMany({
+      include: { 
+        traits: true,
+        shipyard: true,
+        destinations: true,
+      },
+    });
 
     //if no waypoints are found return a 404 error
     if (waypoints.length === 0) {
       return res.status(404).json({ msg: "No waypoints found" });
     }
-    
+
     //If waypoints are found, return a 200 success response with the waypoints data
     return res
       .status(200)
