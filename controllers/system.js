12ab7a0b32cb5132bc1833b81cbc46769f4835e8
mintep1-student <mintep1@student.op.ac.nz>
2024-05-31 12:10:32 +1200

    store filter options and orderBy specs from req.query

--- controllers/system.js
+++ controllers/system.js
@@ -4,12 +4,16 @@ const prisma = new PrismaClient();
 /**
  * Retrieves all systems in the database and returns the system objects
  */
-const getSystems = async (req, res) => {
+const getSystems = async (req, res, include) => {
   try {
-    //fetch all systems from the database
-    const systems = await prisma.system.findMany({
-      include: { waypoints: true },
-    });
+    // Extract query filters if any
+    const filters = req.query.filters ? JSON.parse(req.query.filters) : {};
+
+    // Order by whatever the user specifies
+    const orderBy = req.query.orderBy;
+
+    // fetch all systems from the database
+    const systems = await prisma.system.findMany({});
 
     //if no systems are found return a 404 error
     if (systems.length === 0) {
