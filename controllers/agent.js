1a187ca55d9a046d7235cf52e5d964d20ae1c913
TylorJB <tylorbennett18@gmail.com>
2024-05-31 13:41:11 +1200

    Attempting to fix error in Postman, agent cant be created so queries wont work, pushing to save current code while the errors are being fixed

--- controllers/agent.js
+++ controllers/agent.js
@@ -4,14 +4,18 @@ const prisma = new PrismaClient();
 const getAgents = async (req, res) => {
   try {
     const query = {
-     username, credits, ships, contracts, shipCount,           
-     include: {
+      // username,
+      // credits,
+      // ships,
+      // contracts,
+      // shipCount,
+      include: {
         ships: true,
         contracts: true,
       },
     };
 
-    if (username || credits || ships || contracts || shipCount) {
+    if (req.query.username || req.query.credits ||  req.query.shipCount) {
       query.where = {
         username: {
           equals: req.query.username || undefined,
@@ -26,24 +30,30 @@ const getAgents = async (req, res) => {
           equals: req.query.contracts || undefined,
         },
         shipCount: {
-          equals: req.query.contracts || undefined,
-        }
+          equals: req.query.shipCount || undefined,
+        },
       };
-    }  
-      const agents = await prisma.user.findMany(query);
-
-        //if no agents are found return a 404 error
-      if (agents.length === 0) {
-        return res.status(404).json({ msg: "No agents found" });
-      }
-      //If agents are found, return a 200 success response with the agents data
-      return res.status(200).json({ msg: "Successfully fetched all agents", data: agents });
-
-      // Handle errors by returning a 500 error with the error message
-    } catch (err) {
-      return res.status(500).json({
-        msg: err.message,
-      });
     }
-  };
+    const agents = await prisma.user.findMany(query);
+
+    //if no agents are found return a 404 error
+    if (agents.length === 0) {
+      return res.status(404).json({ msg: "No agents found" });
+    }
+
+    if (agents.contracts === 0) {
+      return res.status(404).json({ msg: "No contracts found" });
+    }
+    //If agents are found, return a 200 success response with the agents data
+    return res
+      .status(200)
+      .json({ msg: "Successfully fetched all agents", data: agents });
+
+    // Handle errors by returning a 500 error with the error message
+  } catch (err) {
+    return res.status(500).json({
+      msg: err.message,
+    });
+  }
+};
 export { getAgents };
