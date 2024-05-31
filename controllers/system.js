987c696cc0abd226c3796642d94b4c8fd7caa87c
mintep1-student <mintep1@student.op.ac.nz>
2024-05-31 12:12:51 +1200

    Create new query object based on given filters in get all systems

--- controllers/system.js
+++ controllers/system.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 /**
  * Retrieves all systems in the database and returns the system objects
  */
-const getSystems = async (req, res, include) => {
+const getSystems = async (req, res) => {
   try {
     // Extract query filters if any
     const filters = req.query.filters ? JSON.parse(req.query.filters) : {};
@@ -12,8 +12,15 @@ const getSystems = async (req, res, include) => {
     // Order by whatever the user specifies
     const orderBy = req.query.orderBy;
 
+    // Change query to include filters, order, and waypoint objects
+    const query = {
+      where: filters,
+      orderBy: orderBy ? JSON.parse(orderBy) : undefined,
+      include: { waypoints: true },
+    };
+
     // fetch all systems from the database
-    const systems = await prisma.system.findMany({});
+    const systems = await prisma.system.findMany(query);
 
     //if no systems are found return a 404 error
     if (systems.length === 0) {
