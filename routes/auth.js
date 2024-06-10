b61d9fa84532d22b5697fe132b66f001339e853f
mintep1-student <mintep1@student.op.ac.nz>
2024-06-10 17:42:34 +1200

    get user and fix create contracts function, correctly implement login function in auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import express from "express";
 
-import { register } from "../controllers/auth.js";
+import { register, login } from "../controllers/auth.js";
 // import validation here
 
 const router = express.Router();
 
 router.post("/register", (req, res) => register(req, res));
-router.post("/login", (req, res) => login(req, res));
+router.route("/login").post(login);
 
 export default router;
